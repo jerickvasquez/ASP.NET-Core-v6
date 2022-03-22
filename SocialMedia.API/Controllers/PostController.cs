72ec1c5e0df264dfeccdbc545da2a632f53f40b6
jerickvasquez <jorgeerick446@gmail.com>
2022-03-22 12:27:40 -0500

    Modificando el metodo GetPost()

--- SocialMedia.API/Controllers/PostController.cs
+++ SocialMedia.API/Controllers/PostController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Interfaces;
 using SocialMedia.Infraestructure.Repositories;
 
 namespace SocialMedia.API.Controllers
 {
     public class PostController : Controller
     {
+        private readonly IPostRepository _postRepository;
+        //el atajo ctor para hacer el constructor
+        //Inyeccion de dependencia via constructor
+        public PostController(IPostRepository postRepository)
+        {
+            //Al _postRepository se le va asignar el postRepository que es el que le estan inyectando como dependencia
+            _postRepository = postRepository;
+        }
+
         [Route("")]
         [Route("Post")]
         [Route("Post/Index")]
@@ -14,10 +24,10 @@ namespace SocialMedia.API.Controllers
         }
 
         [Route("Post/getPost")]
-        public IActionResult GetPost()
+        public async Task<IActionResult> GetPost()
         {
 
-            var posts = new PostRepository().GetPosts();
+            var posts = await _postRepository.GetPosts();
 
             //Retorna un status 200, todo funciono como se esperaba
             return Ok(posts);
