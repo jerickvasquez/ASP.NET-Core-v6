ae276e487138a5cf8b40fd863ed1ebd0aefe6314
jerickvasquez <jorgeerick446@gmail.com>
2022-03-22 12:22:19 -0500

    Se resuelven las dependencias

--- SocialMedia.API/Program.cs
+++ SocialMedia.API/Program.cs
@@ -1,4 +1,6 @@
 using SocialMedia.API.Controllers;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infraestructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Se resuelven las dependencias
+//Cada vez que el programa haga uso de la abstraccion IPostRepository se le va a entregar la instancia PostRepository
+builder.Services.AddTransient<IPostRepository, PostRepository>();
 
 var app = builder.Build();
 
